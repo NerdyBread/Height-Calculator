568dbcbbdae5bf1b21ff9f20108219863ad190c3
NerdyBread <49594152+NerdyBread@users.noreply.github.com>
2021-02-09 13:17:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from window import Window
+from cli import CLI
 
 if __name__ == '__main__':
-    main_win = Window(500, 300, "Height Calculator",
-                      "Enter your height:", True)
-    main_win.create_widgets()
-    main_win.run()
+	def makeWindow():
+		main_win = Window(500, 300, "Height Calculator",
+						  "Enter your height:", True)
+		main_win.run()
+	cli = CLI(makeWindow)
+	cli.startMsg()
